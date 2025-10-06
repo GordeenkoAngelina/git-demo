807345592087ecb677e21e17902bd661843489a0
“Angelina <gordangelina@gmail.com>
2025-10-06 21:25:17 +0300

    0610

---
+++ dz06102025.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() { 
+int n;
+std::cout << "pls print your name, surname and bithday  " << std::endl;
+std::cin >> n;
+std::cout << "thank you, you have successfully registered for microloan!" << n << std::endl;
+return 0;
+}
